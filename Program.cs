30f7255490bb2e681dddbbc8591a33c4f4d0604a
MUHAMMAD SHAFAY <117296248+SHAFAY04@users.noreply.github.com>
2026-07-17 15:46:12 +0500

    initialized an empty project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load();
+
+builder.Services.AddControllers();
+
+var dbConnectionString=Environment.GetEnvironmentVariable("DATABASE_URL");
+builder.Services.AddDbContext<AppDbContext>((options)=>options.UseNpgsql(dbConnectionString));
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.MapGet("/hello", () =>
+{
+
+});
+
+app.Run();
